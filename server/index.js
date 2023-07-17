18ed0b9afb084fd04ddef440f562bfeb4490598a
Shrikant-kshatriya <shrikant070920@gmail.com>
2023-07-17 20:28:30 +0530

    token

--- server/index.js
+++ server/index.js
@@ -62,7 +62,7 @@ app.post('/login', async (req, res) => {
                 id: userDoc._id,
             },jwtSecret,{}, (err, token) => {
                 if(err) throw err;
-                res.cookie('token', token).json(userDoc);
+                res.cookie('token', token, {httpOnly: true, secure:true}).json(userDoc);
             });
         }
         else{
